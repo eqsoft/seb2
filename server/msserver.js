fe95402fdd4533b8ec052951e61ae5b66197d49c
Stefan Schneider <schneider@hrz.uni-marburg.de>
2015-11-01 19:14:42 +0100

    added SEB websocket simulation. Open send.html as offline file in a browser and send messages. The local server msserver.js will brodcast it to any other clients.

--- server/msserver.js
+++ server/msserver.js
@@ -3,8 +3,11 @@ var WebSocketServer = require('ws').Server
 , wss = new WebSocketServer({port: 8706});
 wss.on('connection', function(ws) {
 	ws.on('message', function(message) {
-	console.log('received: %s', message);
-});
-ws.send('message server connected');
+		wss.clients.forEach(function each(client) {
+			client.send(message);
+		});
+		console.log('received: %s', message);
+	});
+	ws.send('message server connected');
 });
 console.log('message server started on port '+ port);
