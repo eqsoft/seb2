615ababa54c911475b290a099f483791426f8995
Stefan Schneider <schneider@hrz.uni-marburg.de>
2017-09-01 12:14:52 +0200

    edit linux bin scripts

--- browser/bin/linux/beta.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/mnt/hgfs/firefox/linux/32/beta/firefox -app "../../app/seb.ini" -no-remote -profile ../../../profilelinux32_beta -debug 1 -jsconsole -purgecaches
--- browser/bin/linux/debug.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/mnt/hgfs/firefox/linux/32/stable/firefox -app "../../app/seb.ini" -no-remote -profile ../../../profilelinux32 -debug 1 -jsconsole -purgecaches
