2d588fb33063f0b033f7edce0a5079e21fc05872
Stefan Schneider <schneider@hrz.uni-marburg.de>
2019-03-15 22:31:57 +0100

    edit start script

--- browser/bin/linux/64/dev.sh
+++ browser/bin/linux/64/dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # firefox -silent -profile "../sebProfile64"
-firefox -app "../../../../browser/app/seb.ini" -profile "../sebProfile64" -logfile 1 -debug 1 -config "config.dev.json" -no-remote -purgecaches
+# firefox -app "../../../../browser/app/seb.ini" -profile "../sebProfile64" -logfile 1 -debug 1 -config "config.dev.json" -no-remote -purgecaches
