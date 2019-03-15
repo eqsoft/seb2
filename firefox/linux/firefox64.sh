720017419069eb00ff5d2f87e0a149d6214bf3cc
Stefan Schneider <schneider@hrz.uni-marburg.de>
2019-03-15 20:59:49 +0100

    streamline firefox scripts with standalone branch

--- firefox/linux/firefox64.sh
+++ firefox/linux/firefox64.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+#if [ ! -f "64/mozilla.cfg" ] ; then
+	cp ../autoconfig/mozilla.cfg 64/
+#fi
+
+if [ ! -f "64/defaults/pref/autoconfig.js" ] ; then
+	cp ../autoconfig/defaults/pref/autoconfig.js 64/defaults/pref/
+fi
+
 ./64/firefox -profile firefoxProfile64
